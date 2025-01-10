b8434b37b02018b341eebd3e413e535b40e57cdc
Talha <talhahaider074@gmail.com>
2025-01-10 23:29:55 +0500

    changes done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -21,21 +21,20 @@ const HomePage = () => {
 
             <DashboardNav />
 
-            <p className='text-[#000] text-lg block md:hidden my-5 font-medium'>Welcome, Stuart</p>
+            <p className='text-[#000] text-lg my-5 font-medium'>Welcome, Stuart</p>
 
-            <div className='flex items-start gap-x-3 md:mt-[2rem] flex-wrap'>
+            <div className='flex items-start gap-x-3 md:mt-[0rem] flex-wrap'>
 
                 <div className='w-[25rem] mt-2'>
 
                     <div className='w-[100%] bg-[#FAF8F8] rounded-3xl shadow-xl border border-[#FFFEFE] py-3 px-5'>
 
                         <div className='flex justify-between items-center mt-3 sm:mt-7'>
-                            <p className='text-[#a7a7a7] text-lg hidden md:block font-medium'>Welcome, Stuart</p>
-                            <p className='text-[#a7a7a7] md:hidden block'>Total Balance </p>
+                            <p className='text-[#a7a7a7] '>Total Balance </p>
                             <MdOutlineArrowOutward className='text-xl' />
                         </div>
 
-                        <p className='text-[#a7a7a7] text-lg hidden md:block mt-3'>Total Balance ($USD)</p>
+
                         <div className='flex items-center gap-x-2 md:mt-1'>
                             <h1 className='text-2xl font-semibold '>$327,591,814,610</h1>
                             <FaEye className='pr-1 md:text-black text-[#FF8A65] text-xl'
@@ -135,7 +134,7 @@ const HomePage = () => {
                 </div>
 
                 <div className='mt-2 flex-1 '>
-                    <div className='w-[100%] bg-[#FAF8F8] rounded-3xl shadow-xl border border-[#FFFEFE] py-3 px-5 h-[17rem] flex justify-center items-center'>
+                    <div className='w-[100%] bg-[#FAF8F8] rounded-3xl shadow-xl border border-[#FFFEFE] py-3 px-5 h-[14.5rem] flex justify-center items-center'>
                         <img src={Chart} alt="" className='h-[15rem]' />
                     </div>
 
@@ -235,7 +234,7 @@ const HomePage = () => {
 
                     <div className='w-[100%] bg-[#FAF8F8] rounded-3xl shadow-xl border border-[#FFFEFE] py-3 px-5'>
 
-                        <p className='text-lg mt-5'>Buy Credits</p>
+                        <p className='text-lg mt-0'>Buy Credits</p>
                         <p className='text-[#a7a7a7] mt-1 text-sm'><span className='text-black'>Amount</span> - minimum of 1 credit</p>
                         <input type="number" name="" id="" placeholder='0.00' className='h-[2.5rem] rounded-md px-3 mt-2 outline-none bg-white w-[100%]' />
                         <p className='mt-3 text-sm'>Payment Method</p>
